dd60ecfee2c9ad95d496b7aef4a2107955aacf98
Harry Parker <72020025+Parker06@users.noreply.github.com>
2021-11-07 19:47:35 +0000

    updated tables

--- Create table.sql
+++ Create table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE CW1.Customer
 (	
-	CustomerID INT not null, 
+	CustomerID INT IDENTITY(1,1) not null, 
 	CustomerName varchar(40) not null,
 	CustomerAddress varchar(55) not null,
 	Phonenumber varchar(15) not null /*and constraints like no letters*/, 
@@ -11,18 +11,18 @@ CREATE TABLE CW1.Customer
 
 CREATE TABLE CW1.Accommodation
 (	
-	RoomNO varchar(3) not null, 
+	RoomNO int IDENTITY(1,1) not null, 
 	RoomType varchar(20) not null,
 	AmountOfGuests int not null,
-	Maintenance_needed BIT not null /*every 10 visits must trigger to yes*/,
+	Maintenance_needed varchar(20) not null /*every 10 visits must trigger to yes*/,
 	CONSTRAINT pk_Rooms PRIMARY KEY (RoomNO),
 )
 
 CREATE TABLE CW1.CheckIns
 (	
-	CustomerNo INT not null,
+	CustomerNo INT IDENTITY(1,1) not null,
 	Name varchar(40) not null,
-	RoomID varchar(3) not null, 
+	RoomID int IDENTITY(1,1) not null, 
 	RoomType varchar(20) not null,
 	AmountOfGuests INT not null,
     CheckInDate Date not null,
@@ -30,22 +30,38 @@ CREATE TABLE CW1.CheckIns
 	CONSTRAINT pk_CheckRooms PRIMARY KEY (RoomID), 
 )
 
-ALTER TABLE CheckIns
-	ADD CONSTRAINT fk_customer FOREIGN KEY (CustomerNo) REFERENCES CW1.Customer(CustomerID)
-
-ALTER TABLE CheckIns
-	ADD CONSTRAINT fk_room FOREIGN KEY (RoomID) REFERENCES CW1.Accommodation(RoomNO)
-
-
 CREATE TABLE CW1.Maintenance
 (
-    Room varchar(3) not null, 
+    RoomID int IDENTITY(1,1) not null, 
     CheckInDate Date not null,
     CheckOutDate Date not null /*must not be before check in date*/,
     Maintenance_needed BIT not null /*every 10 visits must trigger to yes*/,
-    CONSTRAINT pk_RoomsCleanup PRIMARY KEY (Room), 
+    CONSTRAINT pk_RoomsCleanup PRIMARY KEY (RoomID), 
 )
 
-ALTER TABLE Maintenance
-	ADD CONSTRAINT fk_roomCleanup FOREIGN KEY (Room) REFERENCES CW1.Accommodation(RoomNO)
 
+ALTER TABLE CW1.CheckIns
+	ADD CONSTRAINT fk_customer FOREIGN KEY (CustomerNo) REFERENCES CW1.Customer(CustomerID)
+
+ALTER TABLE CW1.CheckIns
+	ADD CONSTRAINT fk_room FOREIGN KEY (RoomID) REFERENCES CW1.Accommodation(RoomNO)
+
+ALTER TABLE CW1.Maintenance
+	ADD CONSTRAINT fk_roomCleanup FOREIGN KEY (RoomID) REFERENCES CW1.Accommodation(RoomNO)
+
+INSERT INTO CW1.Customer (CustomerName, CustomerAddress, Phonenumber)
+VALUES('Peter Parker', 'New York', '+44 7657 123456') 
+
+INSERT INTO CW1.Accommodation(RoomType, AmountOfGuests, Maintenance_needed)
+VALUES('Bedroom', '2', 'Room Avaliable')
+
+CREATE VIEW CW1.Customer_Booking AS 
+SELECT CW1.Customer.CustomerName, CW1.Customer.CustomerAddress, CW1.Customer.PhoneNumber, CW1.CheckIns.CheckInDate, CW1.CheckIns.CheckOutDate, CW1.Accommodation.RoomNO, CW1.Accommodation.RoomType
+FROM ((CW1.CheckIns
+INNER JOIN CW1.Customer ON CW1.CheckIns.CustomerNo = CW1.Customer.CustomerID)
+INNER JOIN CW1.Accommodation ON CheckIns.RoomID = Accommodation.RoomNO);
+
+SELECT * FROM CW1.Customer_Booking;
+
+INSERT INTO CW1.CheckIns (Name, RoomType, AmountOfGuests, CheckInDate, CheckOutDate)
+VALUES('Peter Parker', 'Bedroom', '2', '2021-11-01', '2021-10-12');
