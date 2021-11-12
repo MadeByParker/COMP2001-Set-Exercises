f1cf4f65ab5b4a47aed400011ef0cdacb790d391
harry-parker6 <72020025+harry-parker6@users.noreply.github.com>
2021-11-12 00:00:40 +0000

    Update Drop_Tables.sql

--- Drop_Tables.sql
+++ Drop_Tables.sql
@@ -1,14 +1,12 @@
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[CW1].[CheckIns]') AND type in (N'U'))
-DROP TABLE [CW1].[CheckIns]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[CW1].[Booking]') AND type in (N'U'))
+DROP TABLE [CW1].[Booking]
 GO
+
 
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[CW1].[Accommodation]') AND type in (N'U'))
 DROP TABLE [CW1].[Accommodation]
 GO
 
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[CW1].[Maintenance]') AND type in (N'U'))
-DROP TABLE [CW1].[Maintenance]
-GO
 
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[CW1].[Customer]') AND type in (N'U'))
 DROP TABLE [CW1].[Customer]
