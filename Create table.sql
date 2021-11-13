d61e601ef2fc836ff3bb85112626066d596ac6a5
harry-parker6 <72020025+harry-parker6@users.noreply.github.com>
2021-11-13 22:15:09 +0000

    Removed Number of Booking to make it easier for trigger

--- Create table.sql
+++ Create table.sql
@@ -9,20 +9,19 @@ CREATE TABLE CW1.Customer
 
 CREATE TABLE CW1.Accommodation
 (	
-	RoomID int  IDENTITY(1,1) not null, 
+	RoomID int IDENTITY(1,1) not null, 
 	RoomType varchar(20) not null,
-	AmountOfGuests int not null,
-	Maintenance_needed varchar(20) not null /*every 10 visits must trigger to yes*/,
-	NoOfBookings int not null,
+	AmountOfGuestsLimit int not null,
+	MaintenanceStatus varchar(20) not null /*every 10 visits must trigger to yes*/,
 	CONSTRAINT pk_Rooms PRIMARY KEY (RoomID),
 )
 
 CREATE TABLE CW1.Booking
 (	
 	BookingID INT IDENTITY(1,1) not null,
-	CustomerID INT not null,
+	CustomerID INT NOT NULL,
 	CustomerName varchar(40) not null,
-	RoomID INT not null, 
+	RoomID INT NOT NULL, 
 	RoomType varchar(20) not null,
 	AmountOfGuests INT not null,
     CheckInDate Date not null,
@@ -32,7 +31,6 @@ CREATE TABLE CW1.Booking
 	REFERENCES CW1.Accommodation(RoomID), 
 	CONSTRAINT Fk_Customer FOREIGN KEY (CustomerID)
 	REFERENCES CW1.Customer(CustomerID),
-
 )
 
 
@@ -41,13 +39,14 @@ INSERT INTO CW1.Customer (CustomerName, CustomerAddress, Phonenumber)
 VALUES('Harry Parker', 'Plymouth', '07455 123456') 
 
 
-INSERT INTO CW1.Accommodation(RoomType, AmountOfGuests, NoOfBookings, Maintenance_needed)
-VALUES('Sheperds Hut', '8', '0', 'Room Avaliable')
-
+INSERT INTO CW1.Accommodation(RoomType, AmountOfGuestsLImit, MaintenanceStatus)
+VALUES('Sheperds Hut', '8', 'Room Avaliable')
 
 
 INSERT INTO CW1.Booking(CustomerID, CustomerName, RoomID, RoomType, AmountOfGuests, CheckInDate, CheckOutDate)
-VALUES ('2', 'Harry Parker', '2', 'Bedroom', '2', '2021-06-26', '2021-07-02')
+VALUES ('1', 'Harry Parker', '4', 'Sheperds Hut', '4', '2021-06-26', '2021-07-02')
 
 SELECT * from CW1.Accommodation
 DELETE FROM CW1.Booking
+
+SELECT * FROM CW1.Accommodation
