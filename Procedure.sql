e4a98a6f6c8423aca37047e23d6e559bc94e7245
harry-parker6 <72020025+harry-parker6@users.noreply.github.com>
2021-11-13 15:51:57 +0000

    updated stored procedure to be more efficient and added template scripts to run

--- Procedure.sql
+++ Procedure.sql
@@ -1,7 +1,8 @@
 CREATE PROCEDURE CW1.AddCustomer(
 	@Name as VARCHAR(255),
 	@Address as VARCHAR(255),
-	@Number as VARCHAR(21)
+	@Number as VARCHAR(21),
+	@responseMessage NVARCHAR(250) OUTPUT
 )
 AS
 BEGIN 
@@ -12,33 +13,28 @@ BEGIN
 			DECLARE @NewName VARCHAR(40)
 			DECLARE @NewAddress VARCHAR(55)
 			DECLARE @NewPhoneNumber VARCHAR(15)
-			DECLARE @responseMessage NVARCHAR(250)
 
-			SELECT @NewPhoneNumber = Phonenumber from CW1.Customer
-			WHERE Phonenumber = @Number
 
-			SELECT @NewName = CustomerName from CW1.Customer
-			WHERE CustomerName = @Name
+			SELECT @NewPhoneNumber = Phonenumber, @NewName = CustomerName, @NewAddress = CustomerAddress from CW1.Customer
+			WHERE Phonenumber = @Number AND CustomerName = @Name AND CustomerAddress = @Address
 
-			SELECT @NewAddress = CustomerAddress from CW1.Customer
-			WHERE CustomerAddress = @Address
 
-			if exists (SELECT * FROM CW1.Customer WHERE CustomerName = @Name AND CustomerAddress = @Address AND Phonenumber = @Number)
-			SET @responseMessage = ' Booking exists with the details: ';
+			IF @NewName IS NULL AND @NewAddress IS NULL AND @NewPhoneNumber IS NULL
+			BEGIN
+				INSERT INTO CW1.Customer
+				VALUES(@Name, @Address, @Number)
+				SET @responseMessage = 'Customer added with the details: ' + @Name + ', ' + @Address + ', '  + @Number;
+			END
 			
 
-		ELSE
+			ELSE
 			BEGIN
-			INSERT INTO CW1.Customer
-			VALUES(@Name, @Address, @Number)
-		
+				SET @responseMessage = 'Customer exists with the details: ' + @Name + ', ' + @Address + ', ' + @Number;
 			END
-
-				
 			IF @@TRANCOUNT > 0 COMMIT;
 		END TRY 
 		BEGIN CATCH
-			SET @Error = @Error+': An error was encountered: There cannot be duplicate bookings by the same person'
+			SET @Error = @Error+': An error was encountered: Customer could not be created.'
 			IF @@TRANCOUNT > 0
 				ROLLBACK TRANSACTION;
 			RAISERROR(@Error, 1, 0);
@@ -47,6 +43,10 @@ END
 
 DROP PROCEDURE IF EXISTS CW1.AddCustomer
 
-exec CW1.AddCustomer 'Harry Parker', 'Plymouth', '07123 456543'
+DECLARE @message VARCHAR(250);
+exec CW1.AddCustomer 'Harry Parker', 'Plymouth', '07123 456543', @message OUTPUT;
+SELECT @message;
 
-exec CW1.AddCustomer 'Harry Parke', 'Plymout', '07123 456788'
+DECLARE @message VARCHAR(250);
+exec CW1.AddCustomer 'Harry Park', 'Plymout', '07123 456788', @message OUTPUT;
+SELECT @message;
