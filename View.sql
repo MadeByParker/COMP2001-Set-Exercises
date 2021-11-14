d83cfcc7b4ca2d6ce9cbac766f0df4e86833ea6f
harry-parker6 <72020025+harry-parker6@users.noreply.github.com>
2021-11-14 13:43:23 +0000

    Added DISTINCT keyword. Also used INNER JOINS to join tables

--- View.sql
+++ View.sql
@@ -1,8 +1,10 @@
-CREATE VIEW CW1.BookingView AS 
-SELECT CW1.Customer.CustomerName, CW1.Customer.CustomerAddress, CW1.Customer.PhoneNumber, CW1.Booking.CheckInDate, CW1.Booking.CheckOutDate, CW1.Accommodation.RoomType
-FROM CW1.Booking, CW1.Customer,CW1.Accommodation;
-
 SELECT * FROM CW1.BookingView
 WHERE CW1.BookingView.CustomerName = 'Harry Parker'
 
 DROP VIEW IF EXISTS CW1.BookingView
+
+CREATE VIEW CW1.BookingView AS 
+SELECT DISTINCT CW1.Booking.BookingID, CW1.Customer.CustomerName, CW1.Customer.CustomerAddress, CW1.Customer.Phonenumber, CW1.Booking.RoomType, CW1.Booking.Catering, CW1.Booking.CheckInDate, CW1.Booking.CheckOutDate
+FROM ((CW1.Booking
+INNER JOIN CW1.Customer ON CW1.Booking.CustomerID = CW1.Customer.CustomerID)
+INNER JOIN CW1.Accommodation ON CW1.Booking.RoomID = CW1.Accommodation.RoomID);
