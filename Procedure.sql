ca5083ec59194cc49c9f838b996153844c2c5a63
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-01-03 18:05:55 +0000

    Update Procedure.sql

--- Procedure.sql
+++ Procedure.sql
@@ -1,40 +1,39 @@
-CREATE PROCEDURE CW1.AddCustomer(
-	@Name as VARCHAR(255),
-	@Address as VARCHAR(255),
-	@Number as VARCHAR(21),
+CREATE PROCEDURE CW2.AddNewProgramme(
+	@Programme_Code as VARCHAR(255),
+	@Title as VARCHAR(255),
+	@Year as VARCHAR(1),
 	@responseMessage NVARCHAR(250) OUTPUT
 )
 AS
 BEGIN 
 	BEGIN TRANSACTION
-
 		BEGIN TRY
 			DECLARE @Error NVARCHAR(Max);
-			DECLARE @NewName VARCHAR(40)
-			DECLARE @NewAddress VARCHAR(55)
-			DECLARE @NewPhoneNumber VARCHAR(15)
+			DECLARE @NewProgrammeCode VARCHAR(40)
+			DECLARE @NewTitle VARCHAR(55)
+			DECLARE @NewYear VARCHAR(1)
 
 
-			SELECT @NewPhoneNumber = Phonenumber, @NewName = CustomerName, @NewAddress = CustomerAddress from CW1.Customer
-			WHERE Phonenumber = @Number AND CustomerName = @Name AND CustomerAddress = @Address
+			SELECT @NewProgrammeCode = ProgrammeCode, @NewTitle = ProgrammeTitle, @NewYear = [Year] from CW2.Programmes
+			WHERE ProgrammeCode = @Programme_Code AND ProgrammeTitle = @Title AND [Year] = @Year
 
 
-			IF @NewName IS NULL AND @NewAddress IS NULL AND @NewPhoneNumber IS NULL
+			IF @NewProgrammeCode IS NULL AND @NewTitle IS NULL AND @NewYear IS NULL
 			BEGIN
-				INSERT INTO CW1.Customer
-				VALUES(@Name, @Address, @Number)
-				SET @responseMessage = 'Customer added with the details: ' + @Name + ', ' + @Address + ', '  + @Number;
+				INSERT INTO CW2.Programmes
+				VALUES(@Programme_Code, @Title, @Year)
+				SET @responseMessage = 'Programme added with the details: ' + @Programme_Code + ', ' + @Title + ', '  + @Year;
 			END
 			
 
 			ELSE
 			BEGIN
-				SET @responseMessage = 'Customer exists with the details: ' + @Name + ', ' + @Address + ', ' + @Number;
+				SET @responseMessage = 'Programme exists with the details: ' + @Programme_Code + ', ' + @Title + ', '  + @Year;
 			END
 			IF @@TRANCOUNT > 0 COMMIT;
 		END TRY 
 		BEGIN CATCH
-			SET @Error = @Error+': An error was encountered: Customer could not be created.'
+			SET @Error = @Error+': An error was encountered: Programme could not be created.'
 			IF @@TRANCOUNT > 0
 				ROLLBACK TRANSACTION;
 			RAISERROR(@Error, 1, 0);
@@ -44,9 +43,9 @@ END
 DROP PROCEDURE IF EXISTS CW1.AddCustomer
 
 DECLARE @message VARCHAR(250);
-exec CW1.AddCustomer 'Harry Parker', 'Plymouth', '07123 456543', @message OUTPUT;
+exec CW2.AddNewProgramme 'Data Analysis', 'DA543', '1', @message OUTPUT;
 SELECT @message;
 
 DECLARE @message VARCHAR(250);
-exec CW1.AddCustomer 'Harry Park', 'Plymout', '07123 456788', @message OUTPUT;
+exec CW2.AddNewProgramme 'Computer Architecture', 'COMP1001', '1', @message OUTPUT;
 SELECT @message;
